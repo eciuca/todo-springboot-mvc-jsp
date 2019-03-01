ec073c3d0a0519fd4182795ce7c8cff1f0ae9465
Emanuel Ciuca <emanuel.ciuca@gmail.com>
2019-03-01 22:02:19 +0200

    Initial commit

---
+++ src/test/java/com/github/eciuca/springmvc/todowebapp/TodoWebappApplicationTests.java
@@ -0,0 +1,16 @@
+package com.github.eciuca.springmvc.todowebapp;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class TodoWebappApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}
